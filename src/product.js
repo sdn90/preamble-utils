e091a1c71d079626db6f20ed7d411792ee6d3a92
Steven Nguyen <steven@culturelabs.la>
2015-08-24 21:52:40 -0700

    products

---
+++ src/product.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+
+/* returns array
+ * [
+ *  { name: 'Color', values: ['Red', 'Blue', 'Green'] },
+ *  { name: 'Size', values: ['S', 'M', 'L', 'XL'] }
+ * ]
+ */
+export function uniqueOptions(product) {
+  return product.options.map((option, index) => {
+    let values = _.pluck(product.variants, 'option' + (index + 1));
+    return {
+      name: option,
+      values
+    };
+  });
+}
+
+export function findVariant(product, options) {
+  return _.findWhere(product.variants, options);
+}
+
+export function imageUrl(url, size) {
+  let re = /\/products\/(.*)\.(.*)/;
+  return url.replace(re, (match, p1, p2) => {
+    return `/products/${p1}_${size}.${p2}`;
+  });
+}
+
---
+++ test/product.js
@@ -0,0 +1,46 @@
+import expect from 'expect';
+import { imageUrl } from '../src/product';
+
+let product = {
+  "variants": [{
+    "id": 1091557361,
+    "title": "Black",
+    "options": ["Black"],
+    "option1": "Black",
+    "option2": null,
+    "option3": null,
+    "price": 1799,
+  }, {
+    "id": 1091557365,
+    "title": "Dark Pink",
+    "options": ["Dark Pink"],
+    "option1": "Dark Pink",
+    "option2": null,
+    "option3": null,
+    "price": 1799,
+  }, {
+    "id": 1091557369,
+    "title": "Blue",
+    "options": ["Blue"],
+    "option1": "Blue",
+    "option2": null,
+    "option3": null,
+  }, {
+    "id": 1091557373,
+    "title": "Red Plum",
+    "options": ["Red Plum"],
+    "option1": "Red Plum",
+    "option2": null,
+    "option3": null,
+    "price": 1799,
+  }],
+  "images": [ "//cdn.shopify.com/s/files/1/0778/8307/products/dummy_image.jpeg?v=1424572403"],
+  "options": ["Color"],
+}
+
+describe('Image URL', () => {
+  it('should append string to filename', () => {
+    expect(imageUrl(product.images[0], 'grande'))
+    .toBe('//cdn.shopify.com/s/files/1/0778/8307/products/dummy_image_grande.jpeg?v=1424572403');
+  });
+});
