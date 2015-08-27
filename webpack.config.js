e81b84163e1a51fa4aa2a0d9209cfca3cbfdd9e6
Steven Nguyen <steven@culturelabs.la>
2015-08-26 17:55:40 -0700

    build tool bullshit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+    ]
+  },
+  output: {
+    libraryTarget: "umd",
+    library: "Preamble"
+  },
+  resolve: {
+    extensions: ['', '.js']
+  }
+}
