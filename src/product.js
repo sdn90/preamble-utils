5a89ef2195e38d0d1877da0158736d97f9dc2476
Steven Nguyen <steven@culturelabs.la>
2015-08-29 17:08:02 -0700

    remove lowestPrice from index file

--- src/product.js
+++ src/product.js
@@ -1,12 +1,10 @@
 import findVariant from './product/findVariant';
 import imageUrl from './product/imageUrl';
-import lowestPrice from './product/lowestPrice';
 import uniqueOptions from './product/uniqueOptions';
 
 export {
   findVariant,
   imageUrl,
-  lowestPrice,
   uniqueOptions
 }
 
